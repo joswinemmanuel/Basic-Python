660826d42c6eef4ce5b806cfe57a4c78a461c6fb
joswinemmanuel <joswinemmanuel2002@gmail.com>
2022-02-04 21:44:42 +0530

    factorial.py added

---
+++ factorial.py
@@ -0,0 +1,18 @@
+#using for loop
+def fac(n):
+    f=1
+    for i in range(2,n+1):
+        f*=i
+    return f
+
+#using recusion
+def rec_fac(n):
+    if n<=0:
+        return 1
+    else:
+        return n*rec_fac(n-1) 
+
+#Examples
+print(fac(5))
+print(rec_fac(5))
+#120
