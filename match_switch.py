3b146dc3ea833b4254be60df31551940a5c7e413
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-24 09:33:09 +0530

    Add files via upload

---
+++ match_switch.py
@@ -0,0 +1,23 @@
+a=int(input("Enter the any number from 1 to 9 : "))
+
+match a:
+    case 1:
+        print("ONE")
+    case 2:
+        print("TWO")
+    case 3:
+        print("THREE")
+    case 4:
+        print("FOUR")
+    case 5:
+        print("FIVE")
+    case 6:
+        print("SIX")
+    case 7:
+        print("SEVEN")
+    case 8:
+        print("EIGHT")
+    case 9:
+        print("NINE")
+    case _:
+        print("INVALID NUMBER")
