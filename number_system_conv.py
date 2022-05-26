029e7009d3d2b468186e418b9c46eab1f192b4e2
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-26 17:40:55 +0530

    Add files via upload

---
+++ number_system_conv.py
@@ -0,0 +1,16 @@
+deci=int(input("Enter the decimal : "))
+
+print(deci,"in binary",bin(deci)[2:])
+
+print(deci,"in octal",oct(deci)[2:])
+
+print(deci,"in hexadecimal",hex(deci)[2:])
+
+
+binary=input("Enter the binary : ")
+
+print(binary,"in decimal",int(binary,2))
+
+print(binary,"in octal",int(binary,8))
+
+print(binary,"in hexadecimal",int(binary,16))
